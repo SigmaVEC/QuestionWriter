22cc3d1c8e275d6cb5b27a5677c9b1cac10f9cfb
RahulSriram <rahulrockingsriram@gmail.com>
2016-12-30 00:36:29 +0530

    added get questions handler

--- server.go
+++ server.go
@@ -9,12 +9,11 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"io"
-	"fmt"
 )
 
 var (
-	maxQuestions = 50
-	sessionKeyLength=50
+	sessionKeyLength = 50 //In bytes
+	sessionExpiry = 30 //In minutes
 	emptyJson = "{}"
 	db *sql.DB
 	user string = "root"
@@ -24,9 +23,9 @@ var (
 
 type RegisterRequest struct {
 	RegisterNumber string
-	Name           string
-	AcademicYear   int
-	Branch         string
+	Name string
+	AcademicYear int
+	Branch string
 	Semester int
 	CollegeName string
 }
@@ -40,18 +39,55 @@ type QuestionUpdateRequest struct {
 	Answer string
 }
 
+type SubQuestionModel struct {
+	QuestionId int
+	Question string
+	Choice []string
+}
+
+type QuestionModel struct {
+	File string
+	Description string
+	SubQuestion []SubQuestionModel
+}
+
 type QuestionListResponse struct {
 	Expiry int
-	Question []struct {
-		File string
-		Type string
-		Description string
-		SubQuestion []struct {
-			QuestionId int
-			Question string
-			Type string
-			Choice map[int] string
+	Question []QuestionModel
+}
+
+func getQuestion(question string, data QuestionListResponse) (int, error) {
+	for i := 0; i < len(data.Question); i++ {
+		if data.Question[i].Description == question {
+			return i, nil
 		}
+	}
+
+	return -1, error.Error("not found")
+}
+
+func generateSubQuestion(questionId int, subQuestion string) (SubQuestionModel, error) {
+	var subQuestionObject SubQuestionModel
+	dbChoices, err := db.Query("SELECT Choice FROM Choices WHERE QuestionId=?", questionId)
+	defer dbChoices.Close()
+
+	subQuestionObject.QuestionId = questionId
+	subQuestionObject.Question = subQuestion
+
+	if err == nil {
+		if len(dbChoices) != 0 {
+			for dbChoices.Next() {
+				var choice string
+				dbChoices.Scan(&choice)
+				subQuestionObject.Choice = append(subQuestionObject.Choice, choice)
+			}
+		} else {
+			subQuestionObject.Choice = nil
+		}
+
+		return subQuestionObject, nil
+	} else {
+		return nil, err
 	}
 }
 
@@ -93,7 +129,52 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getQuestionsHandler(w http.ResponseWriter, r *http.Request) {
-	//TODO: Get Questions Handler
+	sessionId := r.FormValue("sessionId")
+	dbSession := db.QueryRow("SELECT SessionId FROM Session WHERE SessionId=?", sessionId)
+	err := dbSession.Scan(&sessionId)
+
+	if err == nil && len(sessionId) != 0 {
+		dbQuestions, err := db.Query("SELECT QuestionId, Question, File, SubQuestion FROM Questions");
+		defer dbQuestions.Close()
+
+		if err == nil && len(dbQuestions) != 0 {
+			var reply QuestionListResponse
+			reply.Expiry = sessionExpiry
+
+			for dbQuestions.Next() {
+				var questionId int
+				var data [3]string
+				dbQuestions.Scan(&questionId, &data[0], &data[1], &data[2])
+				i, err := getQuestion(data[0], reply)
+
+				if err == nil {
+					subQuestion, err := generateSubQuestion(questionId, data[2])
+
+					if err == nil {
+						reply.Question[i].SubQuestion = append(reply.Question[i].SubQuestion, subQuestion)
+					} else {
+						io.WriteString(w, emptyJson)
+					}
+				} else {
+					question := QuestionModel{ Description: data[0], File: data[1] }
+					subQuestion, err := generateSubQuestion(questionId, data[2])
+
+					if err == nil {
+						question.SubQuestion = append(question.SubQuestion, subQuestion)
+						reply.Question = append(reply.Question, question)
+					} else {
+						io.WriteString(w, emptyJson)
+					}
+				}
+			}
+
+			json.NewEncoder(w).Encode(reply)
+		} else {
+			io.WriteString(w, emptyJson)
+		}
+	} else {
+		io.WriteString(w, emptyJson)
+	}
 }
 
 func updateQuestionHandler(w http.ResponseWriter, r * http.Request) {
